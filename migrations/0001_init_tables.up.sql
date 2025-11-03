414a227d164b3dde0075f7fc57301b7b312fc259
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2025-11-03 23:39:47 +0500

    feat(db): add migration files

---
+++ migrations/0001_init_tables.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE categories
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TYPE transaction_type AS ENUM ('income', 'expense');
+
+CREATE TABLE transactions
+(
+    id SERIAL PRIMARY KEY,
+    category_id INT REFERENCES categories(id) ON DELETE SET NULL,
+    type transaction_type NOT NULL,
+    amount NUMERIC(12,2) CHECK (amount >= 0) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    transaction_date DATE NOT NULL
+);
